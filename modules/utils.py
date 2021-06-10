4c421ab7407e25825527387f2b15d75d84180de9
Willy-JL <willy.leslie@icloud.com>
2021-06-11 01:24:42 +0200

    Test 3

--- modules/utils.py
+++ modules/utils.py
@@ -259,7 +259,8 @@ async def imgur_image_upload(img: bytes):
                                        "Authorization": f"Client-ID {globals.IMGUR_CLIENT_ID}"
                                    },
                                    data = {
-                                       'image': base64.b64encode(img)
+                                       'image': base64.b64encode(img),
+                                       "type": "base64"
                                    }) as req:
                 resp = await req.json()
         print(resp)
