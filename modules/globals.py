64e1f844141afef0c9673126fccaf50e6b9da96d
Willy-JL <willy.leslie@icloud.com>
2021-11-17 22:19:38 +0100

    Add typehints for IDE suggestions

--- modules/globals.py
+++ modules/globals.py
@@ -1,62 +1,70 @@
+from asyncio import AbstractEventLoop
+from discord.ext.commands import Bot
+from aiohttp import ClientSession
+from aiosqlite import Connection
+from PIL import ImageFont, Image
+from datetime import datetime
+
+
 # System stuff
-bot                      = None
-cur_presence             = None
-db                       = None
-http                     = None
-loop                     = None
-restart_dt               = None
-start_dt                 = None
-ticking_cooldowns        = None
-write_as_token           = None
+bot                      : Bot               = None
+cur_presence             : int               = None
+db                       : Connection        = None
+http                     : ClientSession     = None
+loop                     : AbstractEventLoop = None
+restart_dt               : datetime          = None
+start_dt                 : datetime          = None
+ticking_cooldowns        : bool              = None
+write_as_token           : str               = None
 
 # User configurable vars, Heroku's "Config Vars" are suggested
-ADMIN_ID                 = None
-ASSISTANCE_CATEGORY_IDS  = None
-BLACKLISTED_CHANNELS_IDS = None
-BOT_PREFIX               = None
-CONTRIB_AMOUNT           = None
-CONTRIB_CHANNELS_IDS     = None
-CONTRIB_COOLDOWN         = None
-DAILY_LEVEL_AMOUNT       = None
-DB_HOST_TYPE             = None
-DISCORD_TOKEN            = None
-GITHUB_GIST_FILENAME     = None
-GITHUB_GIST_ID           = None
-GITHUB_GIST_TOKEN        = None
-GITHUB_GIST_USER         = None
-HEROKU_TOKEN             = None
-IMGUR_CLIENT_ID          = None
-JOIN_LOG_CHANNEL_IDS     = None
-LEVEL_NOTIF_CHANNEL_IDS  = None
-MODDER_CATEGORY_IDS      = None
-MODDER_ROLE_IDS          = None
-NO_PERM_ICON             = None
-REP_CRED_AMOUNT          = None
-REP_ICON                 = None
-REQUESTS_CHANNEL_IDS     = None
-REQUESTS_COOLDOWN        = None
-REQUESTS_ICONS           = None
-STAFF_ROLE_IDS           = None
-TROPHY_ROLES             = None
-WRITE_AS_PASS            = None
-WRITE_AS_POST_ID         = None
-WRITE_AS_USER            = None
-XP_AMOUNT                = None
-XP_COOLDOWN              = None
+ADMIN_ID                 : int               = None
+ASSISTANCE_CATEGORY_IDS  : list              = None
+BLACKLISTED_CHANNELS_IDS : list              = None
+BOT_PREFIX               : str               = None
+CONTRIB_AMOUNT           : int               = None
+CONTRIB_CHANNELS_IDS     : list              = None
+CONTRIB_COOLDOWN         : int               = None
+DAILY_LEVEL_AMOUNT       : int               = None
+DB_HOST_TYPE             : str               = None
+DISCORD_TOKEN            : str               = None
+GITHUB_GIST_FILENAME     : str               = None
+GITHUB_GIST_ID           : str               = None
+GITHUB_GIST_TOKEN        : str               = None
+GITHUB_GIST_USER         : str               = None
+HEROKU_TOKEN             : str               = None
+IMGUR_CLIENT_ID          : str               = None
+JOIN_LOG_CHANNEL_IDS     : dict              = None
+LEVEL_NOTIF_CHANNEL_IDS  : dict              = None
+MODDER_CATEGORY_IDS      : list              = None
+MODDER_ROLE_IDS          : list              = None
+NO_PERM_ICON             : str               = None
+REP_CRED_AMOUNT          : int               = None
+REP_ICON                 : str               = None
+REQUESTS_CHANNEL_IDS     : dict              = None
+REQUESTS_COOLDOWN        : int               = None
+REQUESTS_ICONS           : dict              = None
+STAFF_ROLE_IDS           : list              = None
+TROPHY_ROLES             : list              = None
+WRITE_AS_PASS            : str               = None
+WRITE_AS_POST_ID         : str               = None
+WRITE_AS_USER            : str               = None
+XP_AMOUNT                : int               = None
+XP_COOLDOWN              : int               = None
 
 # Image components
-font47                   = None
-font35                   = None
-font30                   = None
-font24                   = None
-font20                   = None
-font16                   = None
-default_avatar           = None
-overlays_default         = None
-overlays_staff           = None
-overlays_admin           = None
-shards_orange            = None
-shards_white             = None
-shards_teal              = None
-bars                     = None
-levelups                 = None
+font47                   : ImageFont         = None
+font35                   : ImageFont         = None
+font30                   : ImageFont         = None
+font24                   : ImageFont         = None
+font20                   : ImageFont         = None
+font16                   : ImageFont         = None
+default_avatar           : Image.Image       = None
+overlays_default         : Image.Image       = None
+overlays_staff           : Image.Image       = None
+overlays_admin           : Image.Image       = None
+shards_orange            : Image.Image       = None
+shards_white             : Image.Image       = None
+shards_teal              : Image.Image       = None
+bars                     : dict              = None
+levelups                 : dict              = None
